240e5039ea67bbcfbe68ac59aa62be1fbc5711bf
Alejandro Marcano <+>
2021-11-01 11:51:33 -0400

    completed assignment

---
+++ app.rb
@@ -0,0 +1,17 @@
+# app.rb
+# puts "Hello World"
+
+# print "hey ale "
+# puts "hey ale"
+# puts "hey ale"
+
+# puts [1,2,3].inspect
+
+
+# p [44,33,22,11]
+
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+puts "Hello World!"
+print "Pass this test, please."
+p [1,2,3]
